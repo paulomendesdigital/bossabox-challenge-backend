f799b452e0e6ba9c90a6bf9f8d98b70456c0bbb1
Paulo Mendes <paulomendesdigital@gmail.com>
2019-08-21 13:06:33 -0300

    config MongoDB

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
 app.use(express.json())
+
+// Config mongoose
+mongoose.connect("mongodb://localhost/bossabox", {useNewUrlParser: true})
+            .then(() => {console.log('MongoDB connected') })
+            .catch((err) => {console.log('MongoDB error connection: ' + err) })
 
 app.use('/', require('./routes/app'))
 
